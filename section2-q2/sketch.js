5c49d4eb8c9fb6ab6b024a1e0e801e444cd968d3
rin1027 <67625089+rin1027@users.noreply.github.com>
2020-10-29 10:29:46 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -3,9 +3,34 @@ function setup() {
   createCanvas(200, 200);
   let size = width / 8;
   noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+  for(let i =0; i<8; i++){
+    for(let j =0; j<8; j++){
+ if((i+j)%2==0){
+   fill(255);
+ }
+
+   else{
+     fill(128);
+   }
+ 
+
+   rect(size*i, size*j, size, size);
+
+   for(let j= 0; j<3; j++){
+     if( (i+j)%2 !=0 ){
+       fill(255, 0, 0);
+       ellipse((size*i+size*(i+1))/2, (size*j+size*(j+1))/2, size,size);
+     }
+   }
+
+    for(let j= 5; j <8; j++){
+     if( (i+j)%2 !=0 ){
+       fill(0);
+       ellipse((size*i+size*(i+1))/2, (size*j+size*(j+1))/2, size,size);
+     }
+   }
     }
-  }
+
+    }
+
 }
