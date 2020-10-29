982fa417fc912e13900e520edbcc1cccbad8c002
rin1027 <67625089+rin1027@users.noreply.github.com>
2020-10-29 10:37:51 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  noFill();
+   for(let i = 0; i < 10; i++){
+     let s=i*10;
+    ellipse(50, 50, s, s);
+     if(i>=5){
+       stroke(255, 0, 0);
+     }
+     else{
+       stroke(0, 0, 255);
   }
+   }
 }
